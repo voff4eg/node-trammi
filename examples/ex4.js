3042c3e59748d5719bd119d64fa327bddd77147a
icebox <albertosantini@gmail.com>
2012-02-09 16:15:29 +0100

    Implement Dijkstra algorithm.

--- examples/ex4.js
+++ examples/ex4.js
@@ -1,8 +1,14 @@
 /*jslint node:true */
 var trammiNet = require('../lib/trammiNet');
 
-// var roads = possibleRoutes(createNet(), '12254', '12249');
-// var roads = possibleRoutes(createNet(), '12254', '11888');
-var roads = trammiNet.possibleRoutes('11631', '11572');
+var roads;
+
+roads = trammiNet.findPath('11631', '11572');
+console.log(roads);
+
+roads = trammiNet.findPath('11572', '11500');
+console.log(roads);
+
+roads = trammiNet.findPath('11631', '11500');
 console.log(roads);
 
