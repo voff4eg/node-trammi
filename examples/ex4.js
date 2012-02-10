50b9dc5fa3a9c01b9a8eb96af262f104b8eed5c9
icebox <albertosantini@gmail.com>
2012-02-10 16:25:17 +0100

    Decorate the route with the line.

--- examples/ex4.js
+++ examples/ex4.js
@@ -3,12 +3,15 @@ var trammiNet = require('../lib/trammiNet');
 
 var roads;
 
+console.log('11631 -> 11572');
 roads = trammiNet.findPath('11631', '11572');
 console.log(roads);
 
+console.log('11572 -> 11500');
 roads = trammiNet.findPath('11572', '11500');
 console.log(roads);
 
+console.log('11631 -> 11500');
 roads = trammiNet.findPath('11631', '11500');
 console.log(roads);
 
