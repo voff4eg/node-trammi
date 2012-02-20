3289c026f7a92fc94b7c6bb6ec9a2a5069778991
icebox <albertosantini@gmail.com>
2012-02-20 14:11:13 +0100

    Tweak the search algorithm.

--- examples/ex4.js
+++ examples/ex4.js
@@ -7,11 +7,21 @@ console.log('11631 -> 11572');
 roads = trammiNet.findPath('11631', '11572');
 console.log(roads);
 
-console.log('11572 -> 11500');
-roads = trammiNet.findPath('11572', '11500');
-console.log(roads);
-
-console.log('11631 -> 11500');
-roads = trammiNet.findPath('11631', '11500');
-console.log(roads);
-
+// console.log('11572 -> 11500');
+// roads = trammiNet.findPath('11572', '11500');
+// console.log(roads);
+//
+// console.log('11631 -> 11500');
+// roads = trammiNet.findPath('11631', '11500');
+// console.log(roads);
+//
+// trammiNet.getRoute('lagosta', 'duomo');
+//
+// console.log('12941 -> 11500');
+// roads = trammiNet.findPath('12941', '11500');
+// console.log(roads);
+//
+// console.log('11630 -> 11500');
+// roads = trammiNet.findPath('11630', '11500');
+// console.log(roads);
+//
